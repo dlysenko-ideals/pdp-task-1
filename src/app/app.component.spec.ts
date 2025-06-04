5b3cdccbe408f78f27ef8643eb4860113609c1b3
denys.lysenko <denys.lysenko@idealscorp.com>
2025-06-04 12:14:52 +0300

    Update title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pdp-task-0');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pdp-task-1');
   });
 });
